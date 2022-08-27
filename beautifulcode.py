d47d804de0fd6ae9ec40916a6b933bdb779339a0
Basil Njoga <Basilnjoga@gmail.com>
2022-08-28 00:45:32 +0300

    chec

--- beautifulcode.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-'''
-This is a code that is well written
-
-'''
-
-print("Hello world")
--- models/base_model.py
+++ models/base_model.py
@@ -2,3 +2,13 @@
 '''
 This is a file for all my models
 '''
+class BaseModel:
+    """ defines all commond attributes and methods """
+    def __init__(self,):
+
+    def __str__():
+        return ""
+
+    def save(self):
+    
+    def to_dict(self):
