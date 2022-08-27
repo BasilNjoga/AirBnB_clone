d04e42eb048500cff9ddf55479c50993965af9f7
Basil Njoga <Basilnjoga@gmail.com>
2022-08-28 00:54:38 +0300

    check

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,18 @@
 '''
 This is a file for all my models
 '''
+import uuid
 class BaseModel:
     """ defines all commond attributes and methods """
-    def __init__(self,):
+    def __init__(self,id,created_at, updated_at):
+        self.id = str(uuid.uuid4())
 
-    def __str__():
-        return ""
+
+    def __str__(self):
+        return "[BaseModel] (" + self.id + ")" + self.dict
 
     def save(self):
+        self.updated_at = 
     
     def to_dict(self):
+        return self.dict
