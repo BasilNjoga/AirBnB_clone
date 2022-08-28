8e9a537b7c14261ebf9867e1eb650e1c47a559a7
Basil Njoga <Basilnjoga@gmail.com>
2022-08-29 00:35:02 +0300

    check

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,15 @@
 This is a file for all my models
 '''
 import uuid
+from datetime import datetime
+
 class BaseModel:
     """ defines all commond attributes and methods """
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
 
 
     def __str__(self):
