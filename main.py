5857e4358f97b8a5fe210ff0332cef65c6431420
Basil Njoga <Basilnjoga@gmail.com>
2022-08-30 02:16:37 +0300

    check

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
-from datetime import datetime
 
 bm = BaseModel()
-bm.updated_at = datetime.utcnow()
+bm.save()
+print(type(bm.updated_at))
 d_json = bm.to_dict()
-print(type(d_json))
-print(type(d_json['id']))
 print(type(d_json['updated_at']))
-print(type(d_json['__class__']))
-print(d_json['__class__'])
--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,12 @@ class BaseModel:
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        change = datetime.now()
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns dictionary representation of all key/vlaues of dict"""
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
         mydict = dict(self.__dict__)
-        mydict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        mydict["self.updated_at"] = self.updated_at.strftime(
-            "%Y-%m-%dT%H:%M:%S.%f")
         mydict["__class__"] = type(self).__name__
         return mydict
