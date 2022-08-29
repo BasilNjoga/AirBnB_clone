f6caaf73c334a53fc0c6eb5b51f87f52f6662117
Basil Njoga <Basilnjoga@gmail.com>
2022-08-29 22:58:23 +0300

    check

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,25 @@ This is a file for all my models
 '''
 import uuid
 from datetime import datetime
+from datetime import date
 
 class BaseModel:
     """ defines all commond attributes and methods """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        current = datetime.now()
+        update = datetime.now()
+        self.created_at = current.isoformat()
+        self.updated_at = update.isoformat()
 
 
 
     def __str__(self):
-        return "[BaseModel] (" + self.id + ")" + self.dict
+        return "[BaseModel] (" + self.id + ")"
+
+    def save(self):
+        change = datetime.now()
+        self.updated_at = change.isoformat()
+    
+    def to_dict(self):
+        return dict()
