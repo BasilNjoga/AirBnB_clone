555e919faf022d6639fdce8857f9d604e3e0813b
Basil Njoga <Basilnjoga@gmail.com>
2022-08-30 02:06:42 +0300

    check

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 from datetime import date
 
+
 class BaseModel:
     """ defines all commond attributes and methods """
     def __init__(self, *args, **kwargs):
@@ -13,18 +14,19 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
-        return  f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """Returns string representation of class"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         change = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """ Returns dictionary representation of all key/vlaues of dict"""
         mydict = dict(self.__dict__)
         mydict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        mydict["self.updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        mydict["__class__"] =  type(self).__name__
+        mydict["self.updated_at"] = self.updated_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        mydict["__class__"] = type(self).__name__
         return mydict
