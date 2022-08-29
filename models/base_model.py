1f7746074bd1c56e6bf6c0a4483a25aa04d87cd5
Basil Njoga <Basilnjoga@gmail.com>
2022-08-30 00:02:27 +0300

    check

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from datetime import date
 
 class BaseModel:
     """ defines all commond attributes and methods """
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
