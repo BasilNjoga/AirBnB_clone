5b9484b2f375cfbdf446e206be670aee2158da9a
Basil Njoga <Basilnjoga@gmail.com>
2022-08-30 22:41:11 +0300

    check

---
+++ console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is a module that contains the entry point of the command interpreter """
+
+class HBNBCommand(cmd.Cmd):
+    """ This class defines a new command line concept """
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
